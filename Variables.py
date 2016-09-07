ad8fa5da24b77f9a7284abf6a54229c04789ce38
Guanwen Wang <gai25@wildcats.unh.edu>
2016-09-06 23:52:32 -0400

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+
+print counter
+print miles
+print name
+
+a, b, c = 1, 2, "john"
+
+print a, b, c
+
+str = 'Hello World!'
+
+print str          # Prints complete string
+print str[0]       # Prints first character of the string
+print str[2:5]     # Prints characters starting from 3rd to 5th
+print str[2:]      # Prints string starting from 3rd character
+print str * 2      # Prints string two times
+print str + "TEST" # Prints concatenated string
