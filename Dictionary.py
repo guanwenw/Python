db56e4754af02c5dae9a2a47828cb67adb91a6e9
Guanwen Wang <gai25@wildcats.unh.edu>
2016-09-07 00:13:48 -0400

    Add files via upload

--- Dictionary.py
+++ Dictionary.py
@@ -6,9 +6,13 @@ dict[2]     = "This is two"
 
 tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
 
-
 print dict['one']       # Prints value for 'one' key
 print dict[2]           # Prints value for 2 key
 print tinydict          # Prints complete dictionary
 print tinydict.keys()   # Prints all the keys
 print tinydict.values() # Prints all the values
+
+if 2 in dict:
+   print "2 is available in the given dict"
+else:
+   print "2 is not available in the given dict"
