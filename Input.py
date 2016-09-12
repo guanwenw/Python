9b23ddc06b2d5e925437e990daadef22d744c93f
Guanwen Wang <gai25@wildcats.unh.edu>
2016-09-11 23:50:00 -0400

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+str = raw_input("Enter your input: ");
+print "Received input is : ", str
+
+str = input("Enter your input: ");
+print "Received input is : ", str
+
+# sample input :  [x*5 for x in range(2,10,2)]
+# 1 == 1
+
