422049a590342e8fba47f459fbd756ee849df2ce
Guanwen Wang <gai25@wildcats.unh.edu>
2016-09-13 17:48:50 -0400

    Update AssertStatement.py

--- AssertStatement.py
+++ AssertStatement.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 def KelvinToFahrenheit(Temperature):
-   assert (Temperature >= 0),"Colder than absolute zero!"
-   return ((Temperature-273)*1.8)+32
+    assert (Temperature >= 0),"Colder than absolute zero!"
+    # OR both works in python 2.7
+    # assert Temperature >= 0 ,"Colder than absolute zero!"
+    return ((Temperature-273)*1.8)+32
+   
 print KelvinToFahrenheit(273)
 print int(KelvinToFahrenheit(505.78))
 print KelvinToFahrenheit(-5)
