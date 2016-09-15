2a2e52591c1fe4045a6d33990e9671fce4ffc899
Guanwen Wang <gai25@wildcats.unh.edu>
2016-09-15 00:01:51 -0400

    Add files via upload

---
+++ TestCases.py
@@ -0,0 +1,10 @@
+import unittest
+
+class Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._connection = createExpensiveConnectionObject()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._connection.destroy()
