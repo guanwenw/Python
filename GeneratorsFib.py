f4fa30a4563d3a54a439da54d8a197d21b7e84f2
Guanwen Wang <gai25@wildcats.unh.edu>
2016-09-19 11:35:20 -0400

    Add files via upload

---
+++ GeneratorsFib.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from itertools import islice
+
+def fib():
+    prev, curr = 0, 1
+    while True:
+        yield curr
+        prev, curr = curr, prev + curr
+
+f = fib()
+print list(islice(f, 0, 10))
+print f.next()
+
