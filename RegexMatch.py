6e745d1fabfcee8f134c938b0f394167bd85f803
Guan <gai25@wildcats.unh.edu>
2016-09-28 23:47:53 -0400

    Update RegexMatch.py

--- RegexMatch.py
+++ RegexMatch.py
@@ -12,3 +12,20 @@ if matchObj:
    print "matchObj.group(3) : ", matchObj.group(3)
 else:
    print "No match!!"
+
+# Matching vs Searching
+# No match, but searching found
+   
+matchObj = re.match( r'dogs', line, re.M|re.I)
+
+if matchObj:
+    print "match --> matchObj.group() : ", matchObj.group()
+else:
+    print "No match!!"
+
+searchObj = re.search( r'dogs', line, re.M|re.I)
+
+if searchObj:
+    print "search --> searchObj.group() : ", searchObj.group()
+else:
+    print "Nothing found!!"
